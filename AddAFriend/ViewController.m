b441f2c1787cbc2c066e68a0d469e28f6a2ec928
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 14:21:11 -0400

    first commit.

--- AddAFriend/ViewController.m
+++ AddAFriend/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+// This is the comment.
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
